481bbc69eb7d6a55c1d239f5eb3e15b69cbb868e
matheusFabri <matheus.fabri@viannasempre.com.br>
2023-03-30 15:24:25 -0300

    classnames library installed

--- components/src/Button.js
+++ components/src/Button.js
@@ -1,3 +1,5 @@
+import className from "classnames";
+
 function Button({
   children,
   primary,
@@ -8,7 +10,11 @@ function Button({
   outline,
   rounded,
 }) {
-  return <button>{children}</button>;
+  return (
+    <button className="px-3 py-1.5 border border-blue-500 bg-blue-500 text-white">
+      {children}
+    </button>
+  );
 }
 
 Button.propTypes = {

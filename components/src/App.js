812910769a14a5b8e7baa134942dc07f03605729
matheusFabri <matheus.fabri@viannasempre.com.br>
2023-03-30 14:35:22 -0300

    prop-types library installed

--- components/src/App.js
+++ components/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "./Button";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <Button>Click</Button>
+      </div>
+      <div>
+        <Button>Me!</Button>
+      </div>
+      <div>
+        <Button>Testing</Button>
+      </div>
+      <div>
+        <Button>Children</Button>
+      </div>
+      <div>
+        <Button>Prop</Button>
+      </div>
     </div>
   );
 }

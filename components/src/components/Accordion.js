801205736d56f1348167243262fc9ed1ce7a1d43
matheusFabri <matheus.fabri@viannasempre.com.br>
2023-03-31 15:14:35 -0300

    accordion component done

--- components/src/components/Accordion.js
+++ components/src/components/Accordion.js
@@ -1,20 +1,41 @@
+import { GoChevronDown, GoChevronRight } from "react-icons/go";
 import { useState } from "react";
 
 function Accordion({ items }) {
-  const [expandedIndex, setExpandedIndex] = useState(0);
+  const [expandedIndex, setExpandedIndex] = useState(-1);
 
   const renderedItems = items.map((item, index) => {
     const isExpanded = index === expandedIndex;
 
+    const handleClick = (nextIndex) => {
+      if (nextIndex === expandedIndex) {
+        setExpandedIndex(-1);
+      } else {
+        setExpandedIndex(nextIndex);
+      }
+    };
+
+    const icon = (
+      <span>{isExpanded ? <GoChevronDown /> : <GoChevronRight />}</span>
+    );
+
     return (
       <div key={item.id}>
-        <div>{item.label}</div>
-        {isExpanded && <div>{item.content}</div>}
+        <div
+          className="flex p-3 bg-gray-50 border-b items-center cursor-pointer justify-between"
+          onClick={() => {
+            handleClick(index);
+          }}
+        >
+          {item.label}
+          {icon}
+        </div>
+        {isExpanded && <div className="border-b p-5">{item.content}</div>}
       </div>
     );
   });
 
-  return <div> {renderedItems} </div>;
+  return <div className="border-x border-t rounded"> {renderedItems} </div>;
 }
 
 export default Accordion;
